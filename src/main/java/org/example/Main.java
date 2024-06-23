3e5dc706144dde96cfd1016737354ea98d97cc5f
tiranosaur <tiranosaur88@gmail.com>
2024-06-23 11:30:35 +0300

    chore: add examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,7 @@
 package org.example;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 public class Main {
     public static void main(String[] args) {
-        ColoredCircle coloredCircle1 = new ColoredCircle(new Red());
-        coloredCircle1.draw();
-        ColoredCircle coloredCircle2 = new ColoredCircle(new Green());
-        coloredCircle2.draw();
-    }
-}
 
-interface Color {
-    default String getColor() {
-        return this.getClass().getSimpleName();
-    }
-}
-
-class Red implements Color {
-}
-
-class Green implements Color {
-}
-
-
-@Data
-abstract class ColoredShape {
-    private String color;
-
-    abstract void draw();
-}
-
-@AllArgsConstructor
-class ColoredCircle extends ColoredShape {
-    private Color color;
-
-    @Override
-    void draw() {
-        System.out.println("This circle is " + color.getColor());
     }
 }
