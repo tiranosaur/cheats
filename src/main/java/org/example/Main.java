7e389c98f50ecff697b8bb839649312a9d7c4b8c
tiranosaur <tiranosaur88@gmail.com>
2024-06-23 17:02:45 +0300

    chore: add examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,28 +2,9 @@ package org.example;
 
 import lombok.*;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<String>list = new ArrayList<String>();
-        System.out.println(list.size());
-        System.out.println(list.);
-    }
-}
 
-@AllArgsConstructor
-@ToString
-class First {
-    private String firstName;
-    @ToString.Exclude
-    private String lastName;
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
     }
 }
 
