4cc3b854b7f9bbc7ec8e332ec22ce5c24b060b0a
tiranosaur <tiranosaur88@gmail.com>
2024-06-23 11:22:47 +0300

    chore: add examples

--- gof/behavior/Memento.java
+++
@@ -1,8 +0,0 @@
-package org.example.gof.behavior;
-
-// фиксирует и хранит текущее состояние объекта, чтобы оно легко восстанавливалось. Класс для хранения состояния
-public class Memento {
-    public static void main(String[] args) {
-
-    }
-}
